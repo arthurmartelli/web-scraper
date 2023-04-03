f443854d2d27d3b1be9550c97e8e5a30ff6e5a1c
Arthur Martelli <66151338+ArthurMartelli@users.noreply.github.com>
2023-04-03 10:30:34 -0600

    added documentation

--- web_scraper/__init__.py
+++ web_scraper/__init__.py
@@ -11,6 +11,9 @@ URL: str = "https://techcrunch.com/"
 
 
 def main() -> None:
+    """Goes trough the TechCrunch website and gets
+    the title and link of the recent news"""
+
     response = requests.get(URL)
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -33,11 +36,23 @@ def main() -> None:
 
 
 def print_table(titles: list[str], urls: list[str]) -> None:
+    """Prints the titles and urls scrapped in table format
+
+    #### Args:
+        - titles (list[str]): Titles scrapped from the URL
+        - urls (list[str]): Urls for the articles
+    """
     table = zip(titles, urls)
     print(tabulate(table, headers=["Title", "URL"], tablefmt="fancy_grid"))
 
 
 def print_colored(titles: list[str], urls: list[str]) -> None:
+    """Prints the titles and urls scrapped in colored in the terminal
+
+    #### Args:
+        - titles (list[str]): Titles scrapped from the URL
+        - urls (list[str]): Urls for the articles
+    """
     for title, url in zip(titles, urls):
         title = colored(title, "green", attrs=["bold"])
         url = colored(url, "light_blue", attrs=["underline"])
